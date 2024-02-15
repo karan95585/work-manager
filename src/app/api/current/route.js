6c2c1e1149009033fd82baecacf913b31a31117e
karan95585 <karan.kamani2003@gmail.com>
2024-02-15 14:13:45 +0530

    third commit

--- src/app/api/current/route.js
+++ src/app/api/current/route.js
@@ -1,12 +1,21 @@
 import { NextResponse } from "next/server";
 import jwt from "jsonwebtoken";
 import { User } from "@/models/user";
+
 export async function GET(request) {
   const authToken = request.cookies.get("authToken")?.value;
-  console.log(authToken);
-  const data = jwt.verify(authToken, process.env.JWT_KEY);
-  console.log(data);
+
+  if (!authToken) {
+    return NextResponse.status(401).json({ error: "Unauthorized" });
+  }
+
+  let data;
+  try {
+    data = jwt.verify(authToken, process.env.JWT_KEY);
+  } catch (error) {
+    return NextResponse.status(401).json({ error: "Unauthorized" });
+  }
+
   const user = await User.findById(data._id).select("-password");
-  //   console.log("test");
   return NextResponse.json(user);
 }
